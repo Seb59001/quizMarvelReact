2560f61b70b9133556754875ef1b063ca2325333
Seb59001 <leg.sebastien@gmail.com>
2023-09-26 12:19:36 +0200

    Nouvelles modifications .env

--- src/components/FirebaseBundle/firebase.js
+++ src/components/FirebaseBundle/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const config = {
-    apiKey: "AIzaSyA7_DcikiXcSMRuqaTmIubno9bj7baU0TA",
-    authDomain: "marvel-quiz-12c75.firebaseapp.com",
-    projectId: "marvel-quiz-12c75",
-    storageBucket: "marvel-quiz-12c75.appspot.com",
-    messagingSenderId: "42683572632",
-    appId: "1:42683572632:web:298cd0fe3b8e3af46f8bed"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 class Firebase {
